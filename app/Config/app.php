f2c48d63cfe905b9bbf7845fac2171b1415e86c0
Lucas Vacari <lucasrvacari99@gmail.com>
2026-07-16 20:20:36 -0300

    Revert "chore(config): Update database credentials to beta environment"
    This reverts commit bdef2709a90c1348adc917528b6beb047b1da178.

--- app/Config/app.php
+++ app/Config/app.php
@@ -14,8 +14,8 @@ return [
     // Configurações do banco de dados
     'database' => [
         'host' => 'localhost',
-        'dbname' => 'brooks_construtora_beta',
-        'username' => 'brooks_construtora_beta',
+        'dbname' => 'brooks_construtora',
+        'username' => 'brooks_construtora',
         'password' => 'nSTjm88b!md0%Ysr',
         'charset' => 'utf8mb4',
     ],
